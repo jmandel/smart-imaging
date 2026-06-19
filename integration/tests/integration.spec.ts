a422167e80c303c1276737a8755974106a9209d4
Josh Mandel <jmandel@med.harvard.edu>
2026-06-19 14:40:21 -0500

    Update integration selectors for protocol UI

--- integration/tests/integration.spec.ts
+++ integration/tests/integration.spec.ts
@@ -5,7 +5,7 @@ test.setTimeout(60000)
 test('Approve and view images', async ({ page }) => {
   await page.goto('https://imaging-local.argo.run/app/viewer/');
   await page.getByRole('combobox').selectOption({label: '(Local) SMART Sandbox + Imaging Server'});
-  await page.getByRole('button', { name: 'Connect' }).click();
+  await page.getByRole('button', { name: 'Connect', exact: true }).click();
   await page.getByRole('button', { name: 'Approve' }).click();
 
   const clinicalPanel = await page.getByRole('heading', { name: 'Clinical Data' }).locator('..');
@@ -16,6 +16,6 @@ test('Approve and view images', async ({ page }) => {
   await expect(studyPanel).toContainText("Danae");
 
   await page.keyboard.press('Escape');
-  await expect(studyPanel).toContainText("Imaging Studies");
+  await expect(studyPanel).toContainText("Imaging");
   await expect(studyPanel).toContainText("Upper Extremity");
 });
